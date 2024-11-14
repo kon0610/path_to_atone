17e70d1ee2afe25f52ba6647ece898de61b9ea85
konkon0610 <Pon06pon10@gmail.com>
2024-11-14 14:23:59 +0900

    feat: 入金処理正常系テスト作成

--- spec/requests/payments_spec.rb
+++ spec/requests/payments_spec.rb
@@ -1,25 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe "Payments", type: :request do
-  describe "GET /new" do
-    it "returns http success" do
-      get "/payments/new"
-      expect(response).to have_http_status(:success)
+  let(:valid_attributes) { { amount: 1000 } }
+  describe "POST /payments" do
+    context "リクエストが正しい場合" do
+      it "新しい入金が作成される" do
+        expect {
+          post payments_path, params: { payment: valid_attributes }
+        }.to change(Payment, :count).by(1)
+      end
+
+      it "入金データが正しくデータベースに保存される" do
+        post payments_path, params: { payment: valid_attributes }
+        payment = Payment.last
+        expect(payment.amount).to eq(1000)
+        expect(payment.payment_time).to be_within(1.second).of(Time.current)
+      end
+
+      it "入金後、入金完了画面にリダイレクトされる" do
+        post payments_path, params: { payment: valid_attributes }
+        payment = Payment.last
+        expect(response).to redirect_to(payment_path(payment))
+      end
     end
   end
-
-  describe "GET /create" do
-    it "returns http success" do
-      get "/payments/create"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET /show" do
-    it "returns http success" do
-      get "/payments/show"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
 end
