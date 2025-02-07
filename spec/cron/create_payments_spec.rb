a0dbd7a29a4beed8032dbe6d504adb933af1ab71
konkon0610 <Pon06pon10@gmail.com>
2025-02-07 10:19:29 +0900

    FIXME：箇所の修正

--- spec/cron/create_payments_spec.rb
+++ spec/cron/create_payments_spec.rb
@@ -74,66 +74,49 @@ RSpec.describe Cron::NettingBatch, type: :module do
       )
     end
     context "正常系：1000円の取引で全ての残高が0になる" do
-      before do
+      it '入金,請求、債権、債務の残高が0になっている' do
         Cron::NettingBatch.run
-      end
-      it '入金の残高が0になっている' do
         expect(@receipt.reload.payment_balance).to eq(0)
-      end
-      
-      it '請求の残高が0になっている' do
         expect(@consumer_billing.reload.billing_balance).to eq(0)
-      end
-
-      it '債務の残高が0になっている' do
         expect(@consumer_debt.reload.latest_consumer_debt).to eq(0)
-      end
-
-      it '債権の残高が0になっていること' do
         expect(@consumer_credit1.reload.latest_consumer_credit).to eq(0)
         expect(@consumer_credit2.reload.latest_consumer_credit).to eq(0)
       end
 
       it '相殺イベントが作成されていること' do
-        event1 = ConsumerOffsetEvent.find_by(consumer_debt_id: @consumer_debt.id, consumer_credit_id: @consumer_credit1.id)
-        event2 = ConsumerOffsetEvent.find_by(consumer_debt_id: @consumer_debt.id, consumer_credit_id: @consumer_credit2.id)
-        expect(event1.reload.offset_amount).to eq(300)
-        expect(event2.reload.offset_amount).to eq(700)
-        expect(event1.reload.offset_datetime).to be_within(1.second).of(Time.current)
-        expect(event2.reload.offset_datetime).to be_within(1.second).of(Time.current)
+        expect {
+          Cron::NettingBatch.run
+        }.to change { ConsumerOffsetEvent.count }.by(2)
       end
 
       it 'バッチ履歴が記録されていること' do
-        batch_log = BatchLog.find_by(batch_name: "NettingBatch")
-        expect(batch_log.reload.batch_name).to eq('NettingBatch')
-        expect(batch_log.reload.batch_executed_at).to_not be_nil
+        expect {
+          Cron::NettingBatch.run
+        }.to change { BatchLog.count }.by(1)
       end
     end
 
     context '異常系: 抽出データがない場合の標準出力' do
-      before do
-        Cron::NettingBatch.run
-      end
-
       it "支払ステータスが 0 ではないデータではエラー" do
         @consumer_billing.update!(payment_status: 1) # 条件に合わないように更新
-        @consumer_billing.reload
-        expect { Cron::NettingBatch.run }.to raise_error(SystemExit)
-        expect { Cron::NettingBatch.run }.to output(/⚠️  該当する入金データがありません。処理を中断します。/).to_stdout
+        expect {
+          Cron::NettingBatch.run
+        }.to raise_error(SystemExit).and output(/⚠️  該当する入金データがありません。処理を中断します。/).to_stdout
       end
 
       it "前回のbacth以前のデータが対象の時はエラー" do
         @consumer_debt.update!(netting_datetime: @latest_batch_time) # 直近バッチよりも前に設定
-        @consumer_debt.reload
-        expect { Cron::NettingBatch.run }.to raise_error(SystemExit)
-        expect { Cron::NettingBatch.run }.to output(/⚠️  該当する入金データがありません。処理を中断します。/).to_stdout
+        expect {
+          Cron::NettingBatch.run
+        }.to raise_error(SystemExit).and output(/⚠️  該当する入金データがありません。処理を中断します。/).to_stdout
       end
 
       it "該当入金データがない場合、エラー" do
         # 全てのデータを削除して抽出対象がない状態を作る
         @receipt.destroy!
-        expect { Cron::NettingBatch.run }.to raise_error(SystemExit)
-        expect { Cron::NettingBatch.run }.to output(/⚠️  該当する入金データがありません。処理を中断します。/).to_stdout
+        expect {
+          Cron::NettingBatch.run
+        }.to raise_error(SystemExit).and output(/⚠️  該当する入金データがありません。処理を中断します。/).to_stdout
       end
     end
   end
